4082ed900cb2f9680f3abd84d7c7778310e6308c
Alexandre Tesser <alexandre.tesser@gmail.com>
2021-03-10 20:35:52 -0300

    vistoria.js
    Atualizacao vistoria.js

--- routes/vistoria.js
+++ routes/vistoria.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 // GET method route
-app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.status(200).send({
       mensagem: 'OK - Vistoria'
   })
 });
 
 // POST method route
-app.post('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
   res.status(201).send({
       mensagem: 'OK - POST - Vistoria'
   })
