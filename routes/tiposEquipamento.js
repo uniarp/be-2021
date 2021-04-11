ad3adffb038b0ac76981d376e30d772f35bac5ae
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-04-10 21:46:52 -0300

    Rotas: 5ª bateria de revisões de codigo

--- routes/tiposEquipamento.js
+++ routes/tiposEquipamento.js
@@ -1,60 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET /tipoEquipamentos/ */
-router.get('/', function(req, res, next) {
-    res.status(200).json({
-        mensagem : 'GET tipoEquipamentos',
-        id : 1,
-        nome: 'Impressora',
-        dataAquisicao: '01/03/2021',
-        marca: 'Epson',
-        modelo:'FX-3190'
+/* GET /tiposEquipamento/ */
+router.get('/', function(req, res) {
+    res.status(200).json([
+        {
+            id : 1,
+            nome: "Impressora",
+            dataAquisicao: "01/03/2021",
+            marca: "Epson",
+            modelo:"FX-3190"
         },
         {
-        mensagem : 'GET tipoEquipamento',
-        id : 2,
-        nome: 'Impressora',
-        dataAquisicao: '02/03/2021',
-        marca: 'HP',
-        modelo: 'PRO-3678'
-    })
+            id : 2,
+            nome: "Impressora",
+            dataAquisicao: "02/03/2021",
+            marca: "HP",
+            modelo: "PRO-3678"
+        }
+    ]);
 });
 
-/* POST /tipoEquipamentos/cadastrar */
-router.post('/cadastrar', function(req, res, next) {
-    const tipoEquipamento = req.params.tipoEquipamento
-    res.status(201).send({
-        mensagem : 'POST tipoEquipamentos ',
-        id : 1,
-        nome: 'Impressora',
-        dataAquisicao: '01/03/2021',
-        marca: 'Epson',
-        modelo:'FX-3190'
-    })
-})
-
-/* POST /softwares/{id}/alterar. */
-router.post('/:id_tipoEquipamento/alterar', function(req, res, next) {
-    const id = req.params.id_tipoEquipamento
-    const tipoEquipamento = req.params.tipoEquipamento
-    res.json({
-        mensagem : 'GET Alterar tipoEquipamento',
-        id : 77,
-        nome: 'Impressora',
-        dataAquisicao: '04/04/2021',
-        marca: 'Nova',
-        modelo:'X-90'
-    })
+/* POST /tiposEquipamento/cadastrar */
+router.post('/cadastrar', function(req, res) {
+    const data = {
+        nome : req.body.nomeTipoEquipamento,
+        dataAquisicao : req.body.dataAquisicaoTipoEquipamento,
+        marca : req.body.marcaTipoEquipamento,
+        modelo : req.body.modeloTipoEquipamento
+    };
+    res.status(201).json(data);
 });
 
-/* GET /tipoEquipamentos/{id}/excluir. */
-router.get('/:id_tipoEquipamento/excluir', function(req, res, next) {
-    const id = req.params.id_software
-    res.json({
-        mensagem : 'GET ID excluir TipoEquipamento',
+/* POST /tiposEquipamento/{id}/alterar. */
+router.post('/:id_tipoequipamento/alterar', function(req, res, next) {
+    const id = req.params.id_tipoequipamento
+    const data = {
+        nome : req.body.nomeTipoEquipamento,
+        dataAquisicao : req.body.dataAquisicaoTipoEquipamento,
+        marca : req.body.marcaTipoEquipamento,
+        modelo : req.body.modeloTipoEquipamento
+    };
+    res.status(200).json(data);
+});
+
+/* GET /tiposEquipamento/{id}/excluir. */
+router.get('/:id_tipoequipamento/excluir', function(req, res, next) {
+    const id = req.params.id_tipoequipamento
+    res.status(200).json({
         id : id
-    })
+    });
 });
 
 module.exports = router;
