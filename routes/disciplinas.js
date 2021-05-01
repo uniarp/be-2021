4d6c8ae0b4364d6b3644c37deebacdaaf9522289
Nophaie Viscente Morais <nophaie.morais@GUARAPLY.GUARARAPES-PLYWOOD.COM.BR>
2021-05-01 18:44:28 -0300

    Alterando rotas INSERT, UPDATE, SELECT e DELETE

--- routes/disciplinas.js
+++ routes/disciplinas.js
@@ -46,7 +46,7 @@ router.post('/cadastrar', async(req, res)=> {
 });
 
 /* POST /disciplinas/{id}/alterar */
-router.get('/:id_disciplina/alterar', async(req, res)=> {
+router.get('/:id/alterar', async(req, res)=> {
     try {
         const data = {
             id : req.params.id,
