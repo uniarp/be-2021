053450178901d5cce141517bfa25dc474b2f56be
Nophaie Viscente Morais <nophaie.morais@GUARAPLY.GUARARAPES-PLYWOOD.COM.BR>
2021-05-01 18:22:31 -0300

    Alteração rotas INSERT, UPDATE, SELECT e DELETE

--- routes/disciplinas.js
+++ routes/disciplinas.js
@@ -1,51 +1,88 @@
 var express = require('express');
+const pool = require('../bd');
 var router = express.Router();
 
 /* GET /disciplinas */
-router.get('/', function(req, res) {
-    res.status(200).json([
-        {
-            id: 1,
-            nome : "Matemática Discreta",
-            periodo : "Noturno",
-            diaSemana : [2, 3],
-        },
-        {
-            id: 2,
-            nome : "Projeto Integrador I",
-            periodo : "Noturno",
-            diaSemana : [1, 4]
+router.get('/', async(req, res)=> {
+    try{
+        const data = {
+            nome : req.body.nome,
+            periodo: req.body.periodo,
+            diaSemana : req.body.diasemana,
+            id_professor : req.body.id_professor
         }
-    ]);
+        const query = await pool.query(`SELECT * FROM disciplina disc INNER JOIN professor p
+                                        ON disc.id_professor=p.id`)
+        res.status(200).json(query.rows)
+    } catch (error) {
+        res.status(400).send({
+            mensagem:error.message
+        })
+    }
 });
 
 /* POST /disciplinas/cadastrar */
-router.post('/cadastrar', function(req, res) {
-    const data = {
-        nome : req.body.nomeDisciplina,
-        periodo : req.body.periodoDisciplina,
-        diaSemana : req.body.diaSemanaDisciplina
-    };
-    res.status(201).json(data);
+router.post('/cadastrar', async(req, res)=> {
+    try {
+        const data = {
+            nome : req.body.nome,
+            periodo : req.body.periodo,
+            diaSemana : req.body.diaSemana,
+            id_professor : req.body.id_professor
+        };
+        const query = `INSERT INTO disciplina (nome, periodo, diasemana, id_professor)
+                        VALUES ('${data.nome}','${data.periodo}','${data.diaSemana}',
+                        '${data.id_professor}')`
+        await pool.query(query)
+        console.log(data)
+        res.status(200).send({
+            mensagem : "Cadastro bem sucedido!"
+        })
+    }catch (error) {
+        res.status(400).send({
+            mensagem:error.message
+        })
+    }
 });
 
 /* POST /disciplinas/{id}/alterar */
-router.get('/:id_disciplina/alterar', function(req, res) {
-    const id = req.params.id_disciplina;
-    const data = {
-        nome : req.body.nomeDisciplina,
-        periodo : req.body.periodoDisciplina,
-        diaSemana : req.body.diaSemanaDisciplina
-    };
-    res.status(200).json(data);
+router.get('/:id_disciplina/alterar', async(req, res)=> {
+    try {
+        const data = {
+            id : req.params.id,
+            nome : req.body.nome,
+            periodo : req.body.periodo,
+            diaSemana : req.body.diaSemana,
+            id_professor : req.body.id_professor
+        };
+        const query = `UPDATE disciplina SET nome='${data.nome}',
+                        periodo='${data.periodo}',diasemana='${data.diaSemana}',
+                        id_professor='${data.id_professor}' WHERE id='${data.id}'`;
+        await pool.query(query)
+        res.status(200).send({
+            message:'Disciplina Alterada!'
+        })
+    } catch (error) {
+        res.status(304).send({
+            mensagem:err.message
+        })
+    }
 });
 
 /* GET /disciplinas/{id}/excluir */
-router.get('/:id_disciplina/excluir', function(req, res) {
-    const id = req.params.id_disciplina
-    res.status(200).json({
-        id : id
-    });
+router.get('/:id/excluir', async(req, res)=> {
+    try{
+        const id =req.params.id;
+        const query =`DELETE FROM disciplina WHERE id='${id}'`;
+        await pool.query(query)
+        res.status(200).send({
+            mensagem:"Disciplina Excluída!"
+        });
+    }catch(error){
+        res.status(304).send({
+            mensagem:error.message
+        })
+    }
 });
 
 module.exports = router;
--- routes/entregas.js
+++ routes/entregas.js
@@ -1,47 +1,87 @@
 var express = require('express');
+const pool = require('../bd');
 var router = express.Router();
 
 /* GET /entregas */
-router.get('/', function(req, res){
-    res.status(200).json([
-        {
-            id : 2,
-            data : "21/12/2020",
-            qtd : 9
-        },
-        {
-            id : 3,
-            data : "22/12/2020",
-            qtd : 12
-        }
-    ]);
+router.get('/', async(req, res)=>{
+   try{
+       const query = (`SELECT * FROM entrega ent INNER JOIN professor pro
+                        ON ent.id_professor=pro.id INNER JOIN material mat 
+                        ON ent.id_material=mat.id INNER JOIN usuario usr
+                        ON ent.id_usuario=usr.id`)
+        await pool.query(query)
+        res.status(200).json(query.rows)
+   }catch (error) {
+       res.status(400).send({
+           mensagem: error.message
+       })
+   }
 });
 
 /* POST /entregas/cadastrar */
-router.post('/cadastrar', function(req, res){
-    const data = {
-        data : req.body.dataEntrega,
-        qtd : req.body.qtdEntrega
-    };
-    res.status(201).json(data);
+router.post('/cadastrar', async(req, res)=>{
+    try {
+        const data = {
+            data : req.body.data,
+            qtd : req.body.qtd,
+            id_professor : req.body.id_professor,
+            id_material : req.body.id_material,
+            id_usuario : req.body.id_usuario
+        };
+        const query = `INSERT INTO entrega (data, qtd, id_professor, id_material, id_usuario)
+                        VALUES ('${data.data}','${data.qtd}','${data.id_professor}',
+                        '${data.id_material}','${data.id_usuario}')`
+        await pool.query(query)
+        console.log(data)
+        res.status(200).send({
+            mensagem : "Cadastro bem sucedido!"
+        })
+    } catch (error) {
+        res.status(400).send({
+            mensagem : error.message
+        })
+    }
+});
+
+/* POST /entregas/{id}/excluir */
+router.get('/:id/excluir', async(req, res)=>{
+    try {
+        const id =req.params.id;
+        const query =`DELETE FROM entrega WHERE id='${id}'`;
+        await pool.query(query)
+        res.status(200).send({
+            mensagem:"Entrega excluída com sucesso"
+        });
+    } catch (error) {
+        res.status(304).send({
+            mensagem : error.message
+        })
+    }
 });
 
 /* POST /entregas/{id}/alterar */
-router.get('/:id_entrega/excluir', function(req, res){
-    const id = req.params.id_entrega;
-    const data = {
-        data : req.body.dataEntrega,
-        qtd : req.body.qtdEntrega
+router.post('/:id/alterar',async(req, res)=>{
+    try {
+        const data = { 
+                id : req.params.id_entrega,
+                data: req.body.data,
+                qtd: req.body.qtd,
+                id_professor : req.body.id_professor,
+                id_material : req.body.id_material,
+                id_usuario : req.body.id_usuario
+        };
+        const query = `UPDATE entrega SET data='${data.data}',
+                        qtd='${data.qtd}',id_professor='${data.id_professor}',
+                        id_material='${data.id_material}','${data.id_usuario}' WHERE id='${data.id}'`;
+        await pool.query(query)
+        res.status(200).send({
+            message:'Entrega Alterada!'
+        })
+    } catch (error) {
+        res.status(304).send({
+            mensagem : error.message
+        })
     }
-    res.status(200).json(data);
-});
-
-/* GET /entregas/{id}/excluir */
-router.get('/:id_entrega/alterar',function(req, res){
-    const id = req.params.id_entrega;
-    res.json({
-        id : id
-    });
 });
 
 module.exports=router;
