4f051e43512801bf75fd7b338f634ef2d9952b62
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-05-02 16:34:45 -0300

    Rotas: Adição de EntregasChave e Chaves

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ var salasEspeciaisRouter = require('./routes/salasEspeciais');
 var softwaresRouter = require('./routes/softwares');
 var tiposEquipamentoRouter = require('./routes/tiposEquipamento');
 var materiaisRouter = require('./routes/materiais');
-
- 
+// var chavesRouter = require('./routes/chaves');
+// var entregasChaveRouter = require('./routes/entregasChave'); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,6 +61,8 @@ app.use('/salasEspeciais', salasEspeciaisRouter);
 app.use('/softwares', softwaresRouter);
 app.use('/tiposequipamento', tiposEquipamentoRouter);
 app.use('/materiais', materiaisRouter);
+// app.use('/chaves', chavesRouter);
+// app.use('/entregasChave', entregasChaveRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -72,7 +74,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
