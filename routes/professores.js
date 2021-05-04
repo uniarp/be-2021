49d7ef339fddffea10dc561db101d9076d19f8ff
Ramon Meireles da Silva <ramon.meireles32@gmail.com>
2021-05-03 21:54:23 -0300

    Professores: Conexão com o banco de dados funcional com identação

--- routes/professores.js
+++ routes/professores.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const pool = require('../bd')
+
 /* GET /professores */
 router.get('/', async(req, res)=> {
     try {
         const retorno = await pool.query('select * from professor');
-        res.status(200).json(retorno.rows)
+        res.status(200).json(retorno.rows);
     } catch(erro) {
         res.status(400).send({mensagem: erro.message});
     }
@@ -15,23 +16,21 @@ router.get('/', async(req, res)=> {
 router.post('/cadastrar', async(req, res)=> {
     try {
         const data = {
-        login : req.body.login,
-        senha : req.body.senha,
-        nivel : req.body.nivel,
-        nomeCompleto : req.body.nomeCompleto,
-        email : req.body.email
+            login : req.body.login,
+            senha : req.body.senha,
+            nivel : req.body.nivel,
+            nomeCompleto : req.body.nomeCompleto,
+            email : req.body.email
         };
-        await pool.query("insert into professor(login,senha,nivel,nomeCompleto,email) values ($1,$2,$3,$4,$5)RETURNING *",[data.login,data.senha,data.nivel,data.nomeCompleto,data.email]
-        );
+        await pool.query("insert into professor(login,senha,nivel,nomeCompleto,email) values ($1,$2,$3,$4,$5)RETURNING *",[data.login,data.senha,data.nivel,data.nomeCompleto,data.email]);
         res.status(200).send({
             mensagem: "Professor cadastrado com sucesso"
-        })
-    }catch(err){
-    res.status(400).send({
+        });
+    } catch(err) {
+        res.status(400).send({
             mensagem:err.message
-        })
+        });
     }
-
 });
 
 /* POST /professores/{id}/alterar. */
@@ -44,32 +43,31 @@ router.post('/:id/alterar', async(req, res)=> {
         nomeCompleto : req.body.nomeCompleto,
         email : req.body.email
     }
-    try{
+    try {
         await pool.query("UPDATE professor SET login=$1, senha=$2,nivel=$3,nomecompleto=$4,email=$5 WHERE id=$6",[data.login,data.senha,data.nivel,data.nomeCompleto,data.email,id]);
         res.status(200).send({
             message:'Professor alterado com sucesso'
-        })
-    }catch(err){
+        });
+    } catch(err) {
         res.status(304).send({
             mensagem:err.message
-        })
+        });
     }
 });
 
 
 /* GET /professores/{id}/excluir. */
 router.get('/:id/excluir', async(req, res)=>{
-    const id = req.params.id
-    try{
-        const id=req.params.id
-        const ok =await pool.query('delete from professor where id=$1',[id]);
+    try {
+        const id = req.params.id
+        await pool.query('delete from professor where id=$1',[id]);
         res.status(200).send({
             mensagem:"Professor excluido com sucesso"
         });
-    }catch(error){
+    } catch(error) {
         res.status(304).send({
             mensagem:error.message
-        })
+        });
     }
 });
 
