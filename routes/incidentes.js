0e27d656e8c5d4b914069f7550bb9639d2963755
Ramonms98 <ramon.meireles32@gmail.com>
2021-07-06 22:33:33 -0300

    Corrige /incidentes/cadastrar

--- routes/incidentes.js
+++ routes/incidentes.js
@@ -2,78 +2,80 @@ var express = require("express");
 var router = express.Router();
 const pool = require('../bd');
 /* GET /incidentes/ */
-router.get('/', async (req, res)=> {
-    try{
+router.get('/', async (req, res) => {
+    try {
         const query = await pool.query("select * from incidente a  inner join sala b on a.id_sala= b.id inner join professor c  on a.id_professor = c.id")
         res.status(200).json(query.rows)
-    }catch(error){
+    } catch (error) {
         res.status(400).send({
-            mensagem:error.message
+            mensagem: error.message
         })
     }
 });
 
 
 /* POST /incidentes/cadastrar */
-router.post('/cadastrar', async (req, res)=> {
-    try{
-    const data = {
-        descricao : req.body.descricao,
-        tempoAbertura : req.body.tempoAbertura,
-        status : req.body.status,
-        tempoFechamento : req.body.tempoFechamento,
-        resolucao : req.body.resolucao,
-        id_sala: req.body.id_sala,
-        id_professor: req.body.id_professor
-
-    };
-    await pool.query("insert into incidente (descricao,tempoabertura,status,tempofechamento,resolucao, id_sala,id_professor)values('$1','$2', '$3', '$4', '$5','$6','$7','$8')RETURNING *",[data.descricao,data.tempoAbertura,data.status,data.tempoFechamento,data.resolucao, data.id_sala,data.id_professor])
-    res.status(200).send({
-        mensagem:"Incidente cadastrado com sucesso"
-    })
-}catch(err){
-    res.status(400).send({
-        mensagem:err.message
-    })
-}
+router.post('/cadastrar', async (req, res) => {
+    try {
+        const data = {
+            descricao: req.body.descricao,
+            tempoAbertura: req.body.tempoabertura,
+            status: req.body.status,
+            tempoFechamento: req.body.tempofechamento,
+            resolucao: req.body.resolucao,
+            id_sala: req.body.sala,
+            id_professor: req.body.professor
+        };
+        await pool.query(`insert into incidente (descricao, tempoabertura, status, tempofechamento, resolucao, id_sala, id_professor)
+            values($1,$2,$3,$4,$5,$6,$7)
+            RETURNING *`, [data.descricao, data.tempoAbertura, data.status, data.tempoFechamento, data.resolucao, data.id_sala, data.id_professor]
+        );
+        res.status(200).send({
+            mensagem: "Incidente cadastrado com sucesso"
+        })
+    } catch (err) {
+        res.status(400).send({
+            mensagem: err.message
+        })
+    }
 });
 
 /* POST /incidentes/{id}/alterar. */
-router.post('/:id/alterar', async (req, res)=> {
+router.post('/:id/alterar', async (req, res) => {
     const id = req.params.id;
     const data = {
-        descricao : req.body.descricao,
-        tempoAbertura : req.body.tempoAbertura,
-        status : req.body.status,
-        tempoFechamento : req.body.tempoFechamento,
-        resolucao : req.body.resolucao,
+        descricao: req.body.descricao,
+        tempoAbertura: req.body.tempoAbertura,
+        status: req.body.status,
+        tempoFechamento: req.body.tempoFechamento,
+        resolucao: req.body.resolucao,
         id_sala: req.body.id_sala,
         id_professor: req.body.id_professor
     }
-    try{
-        await pool.query("UPDATE incidente SET descricao =$1, tempoAbertura=$2,status=$3,tempoFechamento=$4,resolucao=$5, id_sala=$6, id_professor=$7 WHERE id=$8",[data.descricao,data.tempoAbertura,data.status,data.tempoFechamento,data.resolucao,data.id_sala, data.id_professor,id]);
+    try {
+        await pool.query("UPDATE incidente SET descricao =$1, tempoAbertura=$2,status=$3,tempoFechamento=$4,resolucao=$5, id_sala=$6, id_professor=$7 WHERE id=$8", [data.descricao, data.tempoAbertura, data.status, data.tempoFechamento, data.resolucao, data.id_sala, data.id_professor, id]);
         res.status(200).send({
-            message:'Incidente alterado com sucesso'
+            message: 'Incidente alterado com sucesso'
         })
-    }catch(err){
+    } catch (err) {
         res.status(304).send({
-            mensagem:err.message
+            mensagem: err.message
         })
     }
 });
 
 /* GET /incidentes/{id}/excluir. */
-router.get('/:id/excluir', async(req, res)=>{
+router.get('/:id/excluir', async (req, res) => {
     const id = req.params.id
-    try{
-        const id=req.params.id
-        await pool.query('delete from incidente where id=$1',[id]);
+    try {
+        const id = req.params.id
+        await pool.query('delete from incidente where id=$1', [id]);
         res.status(200).send({
-            mensagem:"Incidente excluido com sucesso"
+            mensagem: "Incidente excluido com sucesso"
         });
-    }catch(error){
+    } catch (error) {
         res.status(304).send({
-            mensagem:error.message
+            mensagem: error.message
         })
     }
 });
